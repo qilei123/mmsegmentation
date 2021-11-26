032afec452f7ab5d4e427047b347fa48bdff683b
qilei123 <qileimail123@gmail.com>
2021-11-26 10:35:57 -0500

    test

--- configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
+++ configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
@@ -35,7 +35,7 @@ data_root = 'data/td/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (640, 640)
-img_scale = (960, 640)
+img_scale = (960, 540)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
