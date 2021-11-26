b21cab5f7eb93c4331b9f6a280a9fc236b8c1fe5
qilei123 <qileimail123@gmail.com>
2021-11-26 14:58:24 -0500

    test

--- configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
+++ configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
@@ -2,8 +2,8 @@ from PIL.Image import TRANSPOSE
 
 
 norm_cfg = dict(type='SyncBN', requires_grad=True)
-num_classes = 4
-dataset_type = 'TDDataset4'
+num_classes = 3
+dataset_type = 'TDDataset3'
 data_root = 'data/td/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
@@ -129,4 +129,4 @@ lr_config = dict(
 runner = dict(type='IterBasedRunner', max_iters=64000)
 checkpoint_config = dict(by_epoch=False, interval=6400)
 evaluation = dict(interval=6400, metric='mIoU', pre_eval=True)
-work_dir = data_root+'work_dirs/full_segformer_mit-b5_640x640_160k_td_bg_640/'
+work_dir = data_root+'work_dirs/full_segformer_mit-b5_640x640_160k_td_nbg_640/'
