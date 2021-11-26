6240f3667a4b5e8abdc38cb93dc0907c441f9d62
qilei123 <qileimail123@gmail.com>
2021-11-26 14:36:30 -0500

    test

--- configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
+++ configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
@@ -6,7 +6,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (640, 640)
 #img_scale = (2048, 640)
-img_scale = (1920, 1080)
+img_scale = (960, 540)
 model = dict(
     type='EncoderDecoder',
     pretrained='pretrain/mit_b5.pth',
