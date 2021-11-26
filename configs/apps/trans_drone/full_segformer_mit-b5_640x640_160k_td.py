963776ed16659e3a2f8d919e1f1a3961e8edc63a
qilei123 <qileimail123@gmail.com>
2021-11-26 14:58:47 -0500

    test

--- configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
+++ configs/apps/trans_drone/full_segformer_mit-b5_640x640_160k_td.py
@@ -3,7 +3,7 @@ from PIL.Image import TRANSPOSE
 
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 num_classes = 3
-dataset_type = 'TDDataset3'
+dataset_type = 'TDDataset'
 data_root = 'data/td/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
